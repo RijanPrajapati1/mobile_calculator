fecad096249c8079cde0f0b5baeec8ef59248ace
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-12-10 13:23:36 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/rijan/mobile_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rijan.mobile_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_calculator/app.dart';
+
+// final
+// const
+
+// HOT RELOAD
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
